55ef7d126dae81e012c372b63dc96a29e2077a19
zobaer53 <hossain.zobaer11858@gmail.com>
2024-02-15 10:30:20 +0600

    feature -- bottom nav added

---
+++ app/src/main/java/com/zobaer53/banking_ui/components/BottomNavigation.kt
@@ -0,0 +1,8 @@
+package com.zobaer53.banking_ui.components
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigation(
+    val title:String,
+    val icon: ImageVector
+)
